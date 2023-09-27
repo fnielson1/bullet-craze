6c0a134151d51130cc30b33f77414151110614ef
Frank Nielson <f.nielson@gmail.com>
2023-09-26 21:17:15 -0600

    Changed acceleration values

--- characters/Player.gd
+++ characters/Player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const SPEED_CHANGE_RATE = 10
+const ACCELERATE_SPEED = SPEED / 20
+const DECELERATE_SPEED = SPEED / 30
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,18 +14,18 @@ func _physics_process(delta: float) -> void:
 	
 	# Handle Jump.
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = move_toward(velocity.y, -SPEED, SPEED_CHANGE_RATE)
+		velocity.y = move_toward(velocity.y, -SPEED, ACCELERATE_SPEED)
 	elif Input.is_action_pressed("ui_down"):
-		velocity.y = move_toward(velocity.y, SPEED, SPEED_CHANGE_RATE)
+		velocity.y = move_toward(velocity.y, SPEED, ACCELERATE_SPEED)
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED_CHANGE_RATE)
+		velocity.y = move_toward(velocity.y, 0, DECELERATE_SPEED)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED_CHANGE_RATE)
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATE_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED_CHANGE_RATE)
+		velocity.x = move_toward(velocity.x, 0, DECELERATE_SPEED)
 
 	move_and_slide()
